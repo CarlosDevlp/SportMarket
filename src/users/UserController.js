4b3a22a82c6eea3a524ec2b2cc12ed5f378a8630
CarlosDevlp <carloscl94r@gmail.com>
2015-07-02 07:07:51 -0500

    version 1.5

--- src/users/UserController.js
+++ src/users/UserController.js
@@ -5,10 +5,8 @@
 (function(){
 
   MP=angular
-       .module('users',['ngMaterial','ui.bootstrap'])   
-        .controller('MiraPlace',main)
-        .directive('slideable',f_slideable)
-        .directive('slideToggle',f_slideToggle);
+       .module('users',['ngMaterial','ui.bootstrap'])
+        .controller('MiraPlace',main);
   //control 1 (principal)
   function main($scope,$mdSidenav,$mdDialog){
     
@@ -44,10 +42,50 @@
                           this.message="";
                       }
                     };                        
+         $scope.$watch('Catalogue.tags.length', function() {
+                if($scope.Catalogue.tags.length==0)
+                  $scope.Catalogue.polos=$scope.Polos;
 
+              });
         $scope.Catalogue={
               hidden:true,
               tags:[],
+              polos:[],
+              addTag:function(tag){
+                    var repetida=false;
+                    for(var i in this.tags)
+                          if(this.tags[i]==tag){
+                            repetida=true;
+                            break;
+                          } 
+
+                    if(!repetida){
+                      this.tags.push(tag);
+                      this.getPolos();
+                    }
+                    
+                  },
+              getPolos:function(){
+                   this.polos=[];
+                   var polos=$scope.Polos;                   
+                   var distinto=false;
+                  
+                   for(var i=0 in polos)
+                     if(polos[i].club==this.tags[0])                          
+                          this.polos.push(polos[i]);
+
+                     
+                  
+              },
+              mostrar:function(club){
+
+                if(club=="prueba")
+                  this.hidden=true;
+                else
+                  this.hidden=false;
+                
+
+              },
               categories:[{label:"Clubs",subcategories:["Barcelona","Real Madrid"]},
                           {label:"Seasons",subcategories:["Summer","Spring"]},
                           {label:"Offers",subcategories:["50% OFF"]}
@@ -57,71 +95,28 @@
         $scope.Details={ 
               size:{label:"Size",value:["S","M","L","XL"],ind:0}
              ,color:{label:"Color",value:["#6FAE9B","#756FAE","#DBE279"],ind:0}
+              ,slides:[{image:'./img/polos/img1.jpg'},
+                      {image:'./img/polos/img2.jpg'},
+                      {image:'./img/polos/img3.jpg'},
+                      {image:'./img/polos/img4.jpg'},
+                      {image:'./img/polos/img5.jpg'}]
+
             };
         
-        
-              $scope.slides = [
-            {name: 'Not my cat.', url: 'https://farm2.staticflickr.com/1318/5114665665_e55b2c2169_n.jpg'},
-            {name: 'Again, not my cat.', url: 'https://farm2.staticflickr.com/1079/901626554_8bc51ec160_n.jpg'}];
-      
-  }     
+      $scope.Home={
+              slides:[{image:'./img/img1.svg',text:'Adidas'},
+                      {image:'./img/img2.svg',text:'Umbro'}]
 
-  /*directivas del slide show*/
-  function f_slideable() {
-    return {
-        restrict:'C',
-        compile: function (element, attr) {
-            // wrap tag
-            var contents = element.html();
-            element.html('<div class="slideable_content" style="margin:0 !important; padding:0 !important" >' + contents + '</div>');
-
-            return function postLink(scope, element, attrs) {
-                // default properties
-                attrs.duration = (!attrs.duration) ? '1s' : attrs.duration;
-                attrs.easing = (!attrs.easing) ? 'ease-in-out' : attrs.easing;
-                element.css({
-                    'overflow': 'hidden',
-                    'height': '0px',
-                    'transitionProperty': 'height',
-                    'transitionDuration': attrs.duration,
-                    'transitionTimingFunction': attrs.easing
-                });
-            };
-        }
-    };
-}
-
-
-function f_slideToggle() {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            var target, content;
-            
-            attrs.expanded = false;
-            
-            element.bind('click', function() {
-                if (!target) target = document.querySelector(attrs.slideToggle);
-                if (!content) content = target.querySelector('.slideable_content');
-                
-                if(!attrs.expanded) {
-                    content.style.border = '1px solid rgba(0,0,0,0)';
-                    var y = content.clientHeight;
-                    content.style.border = 0;
-                    target.style.height = y + 'px';
-                } else {
-                    target.style.height = '0px';
-                }
-                attrs.expanded = !attrs.expanded;
-            });
-        }
-    }
-}
-
-
-
-
-
+      };
+  //polos
+      $scope.Polos=[
+            {image:'./img/polos/img1.jpg',club:"Real Madrid"},
+            {image:'./img/polos/img2.jpg',club:"CBF"},
+            {image:'./img/polos/img3.jpg',club:"Barcelona"},
+            {image:'./img/polos/img4.jpg',club:"ninguno"},
+            {image:'./img/polos/img5.jpg',club:"prueba"}
+      ];
+}  
   
 })();
 
